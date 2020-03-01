f627a56328dd7753fd95e895374c64ff3448da4b
sora-uzu <naruto07170717@gmail.com>
2020-03-01 08:44:10 +0000

    [FIX]5-9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,25 @@
   //= require bootstrap-sprockets
   //= require_tree .
 
+$(document).ready(function () {
+	$('.jquery').on('click', function(){
+		$(this).css('color','red');
+	});
+});
+
+
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+      transition : 'slide',
+      speed : 1000,
+      easing : 'easeOutQuart',
+      navType : 'block',
+      childrenElementType : 'div',
+      arrows : true,
+      autoPlay : true,
+      autoPlayDuration : 3000,
+      keyboardOnAlways : true,
+      hidePrevious : false
+  });
+});
